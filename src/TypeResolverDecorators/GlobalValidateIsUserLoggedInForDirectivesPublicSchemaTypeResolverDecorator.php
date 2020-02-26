a1833233cb7dcf427b80612d0209b3e266ee6e98
Leo <losoviz@gmail.com>
2020-02-27 00:23:53 +0800

    Moved class across packages

---
+++ src/TypeResolverDecorators/GlobalValidateIsUserLoggedInForDirectivesPublicSchemaTypeResolverDecorator.php
@@ -0,0 +1,30 @@
+<?php
+namespace PoP\UserRolesAccessControl\TypeResolverDecorators;
+
+use PoP\ComponentModel\TypeResolvers\AbstractTypeResolver;
+use PoP\UserState\TypeResolverDecorators\AbstractValidateIsUserLoggedInForDirectivesPublicSchemaTypeResolverDecorator;
+use PoP\UserRolesAccessControl\DirectiveResolvers\ValidateDoesLoggedInUserHaveAnyRoleDirectiveResolver;
+use PoP\UserRolesAccessControl\DirectiveResolvers\ValidateDoesLoggedInUserHaveAnyCapabilityDirectiveResolver;
+
+class GlobalValidateIsUserLoggedInForDirectivesPublicSchemaTypeResolverDecorator extends AbstractValidateIsUserLoggedInForDirectivesPublicSchemaTypeResolverDecorator
+{
+    public static function getClassesToAttachTo(): array
+    {
+        return array(
+            AbstractTypeResolver::class,
+        );
+    }
+
+    /**
+     * Provide the classes for all the directiveResolverClasses that need the "validateIsUserLoggedIn" directive
+     *
+     * @return array
+     */
+    protected function getDirectiveResolverClasses(): array
+    {
+        return [
+            ValidateDoesLoggedInUserHaveAnyRoleDirectiveResolver::class,
+            ValidateDoesLoggedInUserHaveAnyCapabilityDirectiveResolver::class,
+        ];
+    }
+}
